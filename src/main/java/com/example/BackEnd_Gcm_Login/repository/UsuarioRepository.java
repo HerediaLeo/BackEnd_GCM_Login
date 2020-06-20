9ca4d1b1429da370136f43c75cb02ac7dfbf64bf
HerediaLeo <lheredia1195@gmail.com>
2020-06-20 19:00:33 -0300

    [ADD] Método consulta, classe UsuarioRepository

--- src/main/java/com/example/BackEnd_Gcm_Login/repository/UsuarioRepository.java
+++ src/main/java/com/example/BackEnd_Gcm_Login/repository/UsuarioRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.BackEnd_Gcm_Login.dto.ConsultaUsuarioDto;
 import com.example.BackEnd_Gcm_Login.dto.RetornoLoginDto;
 import com.example.BackEnd_Gcm_Login.model.Usuario;
 
@@ -13,12 +14,20 @@ import com.example.BackEnd_Gcm_Login.model.Usuario;
 @Transactional
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	@Query("select new com.cesjf.rs.dto.RetornoLoginDto"
+	@Query("select new com.example.BackEnd_Gcm_Login.dto.RetornoLoginDto"
 			+ "(u.id,u.nome,u.sobrenome) "
 			+ "from Usuario u "
 			+ "where u.login = :login "
 			+ "and u.senha = :senha"
 		)
 	RetornoLoginDto retornoLogin(String login, String senha);
+	
+	
+	@Query("select new com.example.BackEnd_Gcm_Login.dto.ConsultaUsuarioDto"
+			+ "(u.id, u.nome, u.sobrenome, u.endereco, u.nascimento, u.genero)"
+			+ "from Usuario u"
+			+ " where u.id = :id"
+		)
+	ConsultaUsuarioDto consulta(Long id);
 
 }
