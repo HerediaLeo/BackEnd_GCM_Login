7711a57dd92561583b256dcce95192686171d257
HerediaLeo <lheredia1195@gmail.com>
2020-06-20 16:50:19 -0300

    [ADD] Projeto Spring Inicial

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# BackEnd_GCM_Login
+Backend referente ao trabalho final da matéria de GESTÃO DE CONFIGURAÇÃO E MUDANÇA. Este trabalho será referente a um login de usuário.
---
+++ src/main/java/com/example/BackEnd_Gcm_Login/BackEndGcmLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEnd_Gcm_Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGcmLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndGcmLoginApplication.class, args);
+	}
+
+}
