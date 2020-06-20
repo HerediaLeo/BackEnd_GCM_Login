a6556eba78f17244f29e06bdf7580bcc11544707
HerediaLeo <lheredia1195@gmail.com>
2020-06-20 17:32:22 -0300

    [ADD] Criação do pacote resource e classe UsuarioResource.

---
+++ src/main/java/com/example/BackEnd_Gcm_Login/resource/UsuarioResource.java
@@ -0,0 +1,10 @@
+package com.example.BackEnd_Gcm_Login.resource;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Transactional
+public class UsuarioResource {
+
+}
