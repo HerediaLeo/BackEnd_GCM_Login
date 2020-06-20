43097dfd25b6ee66d2782c7564614946702e87f0
igorcooli <igorcooli@gmail.com>
2020-06-20 18:44:40 -0300

    [Add]_Endpoint Login

--- src/main/java/com/example/BackEnd_Gcm_Login/repository/UsuarioRepository.java
+++ src/main/java/com/example/BackEnd_Gcm_Login/repository/UsuarioRepository.java
@@ -1,9 +1,24 @@
 package com.example.BackEnd_Gcm_Login.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import javax.transaction.Transactional;
 
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.BackEnd_Gcm_Login.dto.RetornoLoginDto;
 import com.example.BackEnd_Gcm_Login.model.Usuario;
 
+@Repository
+@Transactional
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	@Query("select new com.cesjf.rs.dto.RetornoLoginDto"
+			+ "(u.id,u.nome,u.sobrenome) "
+			+ "from Usuario u "
+			+ "where u.login = :login "
+			+ "and u.senha = :senha"
+		)
+	RetornoLoginDto retornoLogin(String login, String senha);
 
 }
--- src/main/java/com/example/BackEnd_Gcm_Login/resource/UsuarioResource.java
+++ src/main/java/com/example/BackEnd_Gcm_Login/resource/UsuarioResource.java
@@ -1,10 +1,41 @@
 package com.example.BackEnd_Gcm_Login.resource;
 
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.BackEnd_Gcm_Login.dto.LoginDto;
+import com.example.BackEnd_Gcm_Login.dto.RetornoLoginDto;
+import com.example.BackEnd_Gcm_Login.repository.UsuarioRepository;
+
+@CrossOrigin
 @RestController
-@Transactional
+@RequestMapping("/usuario")
 public class UsuarioResource {
+	
+	@Autowired
+	UsuarioRepository repo;
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> validaLogin(@RequestBody LoginDto body){
+		
+		try {
+			RetornoLoginDto retornoDto = repo.retornoLogin(body.getLogin(), body.getSenha());
+			
+			if(retornoDto != null) {
+				return ResponseEntity.status(HttpStatus.OK).body(retornoDto);
+			}else {
+				return ResponseEntity.status(HttpStatus.OK).body("Usuário ou senha inválidos!");
+			}
+			
+		} catch (Exception e) {
+			throw e;
+		}
+	}
 
 }
